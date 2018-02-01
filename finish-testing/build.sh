63a356705a7080fd37de082e689a3184d425a616
Tobin Yehle <tobinyehle@gmail.com>
2018-02-01 00:58:07 -0700

    figure out how to link with ld

--- finish-testing/build.sh
+++ finish-testing/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-llc -filetype=obj -O2 test.ll && clang test.o -o test.out
+# llc -filetype=obj -O2 test.ll && clang test.o -o test.out
+
+llc test.ll
+as test.s -o test.o
+ld -o test.out -dynamic-linker=/lib64/ld-linux-x86-64.so.2 /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/x86_64-linux-gnu/crti.o test.o -lc /usr/lib/x86_64-linux-gnu/crtn.o
