a12b24a72f40587a222b4dc3ec5c062cfc351e01
Tobin Yehle <tobinyehle@gmail.com>
2018-02-11 10:45:29 -0700

    get an int on the heap example working

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -2,15 +2,21 @@
 
 module Codegen where
 
+import Data.Char (ord)
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Short as BS (toShort)
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
+import Data.Maybe (fromMaybe, listToMaybe)
 import Control.Monad.State
 import Control.Monad.Identity
+import Data.Word (Word32)
 
 import LLVM.AST
+import LLVM.AST.DataLayout
+import LLVM.AST.FunctionAttribute
+import qualified LLVM.AST.IntegerPredicate as IPred
 import qualified LLVM.AST.Global as G
 import qualified LLVM.AST.Constant as C
 import LLVM.AST.AddrSpace (AddrSpace(AddrSpace))
@@ -25,7 +31,60 @@ import LibCDefs
 import Scope
 
 generate :: Prog -> IO ByteString
-generate expr = withContext $ \ctx -> withModuleFromAST ctx (genModule expr) moduleLLVMAssembly
+generate prog = withContext $ \ctx -> withModuleFromAST ctx (genModule prog) moduleLLVMAssembly
+
+
+sample :: IO ByteString
+sample = withContext $ \ctx -> withModuleFromAST ctx ir moduleLLVMAssembly
+  where
+    ir :: Module
+    ir = let globals = map GlobalDefinition . defs . execFreshCodegen $ goFast
+           in defaultModule { moduleName = "main", moduleDefinitions = globals }
+
+    goFast :: FreshCodegen ()
+    goFast = do
+      defineFmtString
+      defineGuard
+      finishFunction malloc
+      finishFunction exit
+      finishFunction printf
+      let count   = ConstantOperand $ C.Int 64 2
+      let ptrsize = ConstantOperand $ C.Int 64 (fromIntegral pointerBits `quot` 8)
+      let tagSize = ConstantOperand $ C.Int 64 4
+
+      -- -- malloc a closure
+      -- envSize <- doInstruction longType $ Mul False False ptrsize count []
+      -- size    <- doInstruction longType $ Add False False envSize tagSize []
+      -- loc <- doInstruction (star charType) $ callExternal malloc [size]
+      --
+      -- -- set the type tag
+      -- locType <- doInstruction (star intType) $ BitCast loc (star intType) []
+      -- addInstruction $ Do $ Store True locType closureTag Nothing 1 []
+
+      -- malloc a number
+      mem <- doInstruction (star charType) $ callGlobal malloc [ConstantOperand $ C.Int 64 8]
+      loc <- doInstruction (star intType) $ BitCast mem (star intType) []
+
+      -- set the type tag
+      addInstruction $ Do $ Store True loc intTag Nothing 0 []
+
+      -- set the value
+      valueLoc <- doInstruction (star intType) $ GetElementPtr True loc [ConstantOperand (C.Int pointerBits 1)] []
+      addInstruction $ Do $ Store True valueLoc (ConstantOperand (C.Int 32 33)) Nothing 0 []
+
+      -- get a reference to the array of pointers
+      -- refsLoc <- doInstruction (star (star intType)) $ Add False False loc tagSize []
+      -- set the function pointer
+      -- put things in the environment
+
+      printOperand loc
+
+      finishBlock (Name "entry") (Do $ Ret Nothing [])
+      finishFunction functionDefaults
+        { G.name = Name "main"
+        , G.parameters = ([], False)
+        , G.returnType = VoidType
+        }
 
 
 newtype CodegenT m a =
@@ -35,7 +94,8 @@ newtype CodegenT m a =
 type Env = Map String Operand
 
 data CodegenState = CodegenState
-  { defs :: [Definition]
+  { defs :: [Global]
+  , blocks :: [BasicBlock]
   , instructions :: [Named Instruction]
   , environment :: Env
   }
@@ -48,8 +108,8 @@ execCodegen = execState . codegenState
 modifyEnvironment :: MonadState CodegenState m => (Env -> Env) -> m ()
 modifyEnvironment f = modify $ \s -> s { environment = f . environment $ s }
 
-appendInstruction :: MonadState CodegenState m => Named Instruction -> m ()
-appendInstruction instr = modify $ \s -> s { instructions = instructions s ++ [instr] }
+addInstruction :: MonadState CodegenState m => Named Instruction -> m ()
+addInstruction instr = modify $ \s -> s { instructions = instr : instructions s }
 
 
 type FreshCodegen = FreshT Codegen
@@ -57,11 +117,50 @@ type FreshCodegen = FreshT Codegen
 execFreshCodegen :: FreshCodegen a -> CodegenState
 execFreshCodegen = flip execCodegen emptyState . flip evalFreshT Map.empty
   where
-    emptyState = CodegenState { defs=[], instructions=[], environment=Map.empty }
+    emptyState = CodegenState { defs=[], blocks=[], instructions=[], environment=Map.empty }
+
+
+pointerBits :: Word32
+pointerBits = fst $ pointerLayouts (defaultDataLayout LittleEndian) ! AddrSpace 0
+
+closureTag, intTag :: Operand
+closureTag = ConstantOperand $ C.Int 32 0
+intTag     = ConstantOperand $ C.Int 32 1
+
+
+buildMain :: [Named Instruction] -> Definition
+buildMain body = GlobalDefinition functionDefaults
+  { G.name = Name "main"
+  , G.parameters = ([], False)
+  , G.returnType = VoidType
+  , G.basicBlocks = [BasicBlock (Name "entry") body (Do $ Ret Nothing [])]
+  }
+
+
+defineGuard :: FreshCodegen ()
+defineGuard = do
+  let actualParam   = (star intType, Name "actual")
+  let expectedParam = (intType, Name "expected")
+
+  actualValue <- doInstruction intType $ Load True (uncurry LocalReference actualParam) Nothing 0 []
+  notEq <- doInstruction boolType $ ICmp IPred.NE actualValue (uncurry LocalReference expectedParam) []
+  finishBlock (Name "entry") $ Do $ CondBr notEq (Name "err") (Name "ok") []
+
+  addInstruction $ Do $ callGlobal exit [ConstantOperand (C.Int 32 1)]
+  finishBlock (Name "err") $ Do $ Unreachable []
+
+  finishBlock (Name "ok") $ Do $ Ret Nothing []
+
+  finishFunction functionDefaults
+    { G.name = Name "guard"
+    , G.parameters = ([uncurry Parameter actualParam [], uncurry Parameter expectedParam []], False)
+    , G.returnType = VoidType
+    , G.functionAttributes = [Right InlineHint]
+    }
 
 
 genModule :: Prog -> Module
-genModule (Prog defs expr) = defaultModule { moduleName = "main", moduleDefinitions = numberFmt : main : externalDefs }
+genModule (Prog defs expr) = defaultModule { moduleName = "main", moduleDefinitions = numberFmt : buildMain body : externalDefs }
   where
     externalDefs :: [Definition]
     externalDefs = map GlobalDefinition [printf, malloc, free]
@@ -75,14 +174,6 @@ genModule (Prog defs expr) = defaultModule { moduleName = "main", moduleDefiniti
       , G.initializer = Just $ C.Array charType [C.Int 8 37, C.Int 8 100, C.Int 8 10, C.Int 8 0]
       }
 
-    main :: Definition
-    main = GlobalDefinition functionDefaults
-      { G.name = Name "main"
-      , G.parameters = ([], False)
-      , G.returnType = VoidType
-      , G.basicBlocks = [BasicBlock (Name "entry") body (Do $ Ret Nothing [])]
-      }
-
     body :: [Named Instruction]
     body = instructions . execFreshCodegen $ synthExpr expr >>= printResult
 
@@ -90,11 +181,50 @@ genModule (Prog defs expr) = defaultModule { moduleName = "main", moduleDefiniti
     printResult reg = do
       let refType = PointerType (ArrayType 4 charType) (AddrSpace 0)
       let fmtArg = ConstantOperand $ C.GetElementPtr True (C.GlobalReference refType (Name "fmt")) [C.Int 32 0, C.Int 32 0]
-      appendInstruction $ Do $ callExternal printf [fmtArg, reg]
+      addInstruction $ Do $ callGlobal printf [fmtArg, reg]
 
 
-synthDef :: Def -> FreshCodegen Operand
+defineFmtString :: FreshCodegen ()
+defineFmtString = modify $ \s -> s { defs = global : defs s }
+  where
+    global = globalVariableDefaults
+      { G.name = Name "fmt"
+      , G.linkage = Private
+      , G.isConstant = True
+      , G.type' = ArrayType 4 charType
+      , G.initializer = Just . C.Array charType . mkString $ "%d\n\NUL"
+      }
+    mkString = map (C.Int 8 . fromIntegral . ord)
+
+
+printOperand :: Operand -> FreshCodegen ()
+printOperand loc = do
+  value <- getNum loc
+  let fmtRef = C.GlobalReference (PointerType (ArrayType 4 charType) (AddrSpace 0)) (Name "fmt")
+  let fmtArg = ConstantOperand $ C.GetElementPtr True fmtRef [C.Int 32 0, C.Int 32 0]
+  addInstruction $ Do $ callGlobal printf [fmtArg, value]
+  return ()
+
+
+synthDef :: Def -> FreshCodegen Global
 synthDef (Def name argNames body) = undefined
+
+
+--         i32*                     i32
+getNum :: Operand -> FreshCodegen Operand
+getNum loc = do
+  checkTag loc intTag
+  valueLoc <- doInstruction (star intType) $ GetElementPtr True loc [ConstantOperand (C.Int pointerBits 1)] []
+  doInstruction intType $ Load True valueLoc Nothing 0 []
+
+
+checkTag :: Operand -> Operand -> FreshCodegen ()
+checkTag actual expected = do
+  guardDef <- findGuard
+  addInstruction $ Do $ callGlobal guardDef [actual, expected]
+  where
+    findGuard = gets $ fromMaybe (error "no gurad definition found") . listToMaybe . filter ((Name "guard" ==) . G.name) . defs
+    matches name def = G.name def == name
 
 
 synthExpr :: Expr -> FreshCodegen Operand
@@ -127,11 +257,34 @@ synthExpr (GetEnv name clos) = undefined
 doInstruction :: Type -> Instruction -> FreshCodegen Operand
 doInstruction typ instr = do
   myName <- fresh
-  modify $ \s -> s { instructions = instructions s ++ [myName := instr] }
+  addInstruction $ myName := instr
   return $ LocalReference typ myName
 
 
-callExternal :: Global -> [Operand] -> Instruction
-callExternal func argOps = Call Nothing C [] (globalFOp func) args [] []
+finishBlock :: Name -> Named Terminator -> FreshCodegen ()
+finishBlock name term = do
+  instrs <- gets $ reverse . instructions
+  let block = BasicBlock name instrs term
+  modify $ \s -> s { blocks = block : blocks s, instructions = [] }
+
+
+finishFunction :: Global -> FreshCodegen ()
+finishFunction defaults = do
+  blocks <- gets $ reverse . blocks
+  let def = defaults { G.basicBlocks = blocks }
+  modify $ \s -> s { blocks = [], defs = def : defs s }
+
+
+callGlobal :: Global -> [Operand] -> Instruction
+callGlobal func argOps = Call Nothing cc retAttrs fOp args fAttrs []
   where
+    cc = G.callingConvention func
+    retAttrs = G.returnAttributes func
+    fAttrs = G.functionAttributes func
     args = map (flip (,) []) argOps
+    fOp = Right $ ConstantOperand $ C.GlobalReference ptrType (G.name func)
+    ptrType = PointerType (FunctionType retType argTypes varArgs) (AddrSpace 0)
+    retType = G.returnType func
+    argTypes = map typeOf . fst . G.parameters $ func
+    typeOf (Parameter typ _ _) = typ
+    varArgs = snd . G.parameters $ func
--- src/LibCDefs.hs
+++ src/LibCDefs.hs
@@ -4,11 +4,13 @@ module LibCDefs where
 
 import Data.ByteString.Short (ShortByteString)
 
-import LLVM.AST
+import LLVM.AST hiding (functionAttributes)
 import LLVM.AST.Global
 import LLVM.AST.Constant
 import LLVM.AST.AddrSpace
+import LLVM.AST.FunctionAttribute
 
+{-# ANN module ("HLint: ignore Unnecessary hiding" :: String) #-}
 
 data ExternalDefinition = ExternalDefinition Definition CallableOperand
 
@@ -25,16 +27,18 @@ longType = IntegerType 64
 charType :: Type
 charType = IntegerType 8
 
-charStarType :: Type
-charStarType = PointerType charType (AddrSpace 0)
+boolType :: Type
+boolType = IntegerType 1
+
+star :: Type -> Type
+star t = PointerType t (AddrSpace 0)
 
 
 printf :: Global
 printf = functionDefaults
   { name = Name "printf"
-  , parameters = ([Parameter charStarType (Name "fmt") []], True)
+  , parameters = ([Parameter (star charType) (Name "fmt") []], True)
   , returnType = intType
-  , basicBlocks = []
   }
 
 
@@ -42,25 +46,22 @@ malloc :: Global
 malloc = functionDefaults
   { name = Name "malloc"
   , parameters = ([Parameter longType (Name "size") []], False)
-  , returnType = charStarType
-  , basicBlocks = []
+  , returnType = star charType
   }
 
 
 free :: Global
 free = functionDefaults
   { name = Name "free"
-  , parameters = ([Parameter charStarType (Name "ptr") []], False)
+  , parameters = ([Parameter (star charType) (Name "ptr") []], False)
   , returnType = VoidType
-  , basicBlocks = []
   }
 
 
-globalFOp :: Global -> CallableOperand
-globalFOp def = Right $ ConstantOperand $ GlobalReference ptrType (name def)
-  where
-    ptrType = PointerType (FunctionType retType argTypes varArgs) (AddrSpace 0)
-    retType = returnType def
-    argTypes = map typeOf . fst . parameters $ def
-    typeOf (Parameter typ _ _) = typ
-    varArgs = snd . parameters $ def
+exit :: Global
+exit = functionDefaults
+  { name = Name "exit"
+  , parameters = ([Parameter intType (Name "exit_code") []], False)
+  , returnType = VoidType
+  , functionAttributes = [Right NoReturn, Right NoUnwind]
+  }
