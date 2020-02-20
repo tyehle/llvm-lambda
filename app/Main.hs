7ad15c5e2288e591556b22309eb8c4ab77e7234c
Tobin Yehle <tobinyehle@gmail.com>
2020-02-20 11:08:41 -0500

    factor main module and allow stdin parsing using ./pipeline

--- app/Main.hs
+++ app/Main.hs
@@ -2,28 +2,41 @@ module Main where
 
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.Set as Set
+import System.Environment (getArgs)
 
+import Codegen
 import Expr
 import qualified LowLevel as LL
-import Codegen
+import Parsing
 
-main :: IO ()
--- main = sample >>= BS.putStrLn
-main = generate (LL.runConvert ifs globals) >>= BS.putStrLn
+
+getInput :: IO String
+getInput = do
+  args <- getArgs
+  case args of
+    (arg:_) -> pure arg
+    [] -> getContents
+
+
+runPipeline :: String -> IO BS.ByteString
+runPipeline input = generate (LL.runConvert (either error id $ parse input) globals)
   where
     globals = Set.fromList ["printf"]
-    -- simple = Plus (Nat 1) (Nat 2)
-    -- ast = Let "x" (Plus (Nat 1) (Nat 2)) $ Minus (Nat 5) (Ref "x")
-    -- closure ast
-    -- ((let (x 319) ((lambda (y) (+ x y)) x)) (let (x 319) ((lambda (y) (+ x y)) x)))
-    closures = Let "x" (Nat 319) $ App (Lambda ["y"] (Plus (Ref "x") (Ref "y"))) [Ref "x"]
-    -- (let (mk-add (lambda (n) (lambda (x) (+ x n))))
-    --   (let (a (mk-add 1))
-    --     (let (b (mk-add 2))
-    --       (+ (a 3) (b 5)))))
-    complex = Let "mk-add" (Lambda ["n"] (Lambda ["x"] (Plus (Ref "x") (Ref "n")))) $
-                Let "a" (App (Ref "mk-add") [Nat 1]) $
-                  Let "b" (App (Ref "mk-add") [Nat 2]) $
-                    Plus (App (Ref "a") [Nat 3]) (App (Ref "b") [Nat 5])
-    -- (if0 0 1 2)
-    ifs = If0 (Nat 0) (Nat 1) (Nat 2)
+
+
+main :: IO ()
+main = do
+  input <- getInput
+  runPipeline input >>= BS.putStrLn
+
+
+simple, ast, closures, ifs :: String
+simple = "(+ 1 2)"
+ast = "(let (x (+ 1 2)) (- 5 x))"
+closures = "(let (x 319) ((lambda (y) (+ x y)) x))"
+complex = "(let (mk-add (lambda (n) (lambda (x) (+ x n)))) \
+          \  (let (a (mk-add 1)) \
+          \    (let (b (mk-add 2)) \
+          \      (+ (a 3) (b 5)))))"
+ifs = "(if0 0 1 2)"
+
--- pipeline.sh
+++ pipeline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-stack build && stack exec llvm-lambda-exe > gen.ll && llc-5.0 -filetype=asm -O2 gen.ll && clang gen.s -o gen.out && ./gen.out
+stack run -- "$@" > gen.ll && llc-5.0 -filetype=asm -O2 gen.ll && clang gen.s -o gen.out && ./gen.out
