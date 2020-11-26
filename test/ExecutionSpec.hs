8ab283b242eedb9e1badd7287c8118bc4e7bf763
Tobin Yehle <tobinyehle@gmail.com>
2020-11-25 22:52:07 -0700

    use cmd line in execution tests

--- test/ExecutionSpec.hs
+++ test/ExecutionSpec.hs
@@ -1,52 +1,24 @@
 module ExecutionSpec (findExecutionTests) where
 
--- import qualified Data.AttoLisp as L
-import qualified Data.ByteString.Char8 as BS
--- import qualified Data.ByteString.Lazy as LBS
-import qualified Data.ByteString.UTF8 as BSU
-import qualified Data.ByteString.Lazy.UTF8 as BLU
-import qualified Data.Map as Map
-import qualified Data.Set as Set
-import System.Directory (removeFile)
+import qualified Data.ByteString.Lazy as BL
 import System.FilePath (takeBaseName, replaceExtension)
-import System.Process (readProcess)
 import Test.Tasty
 import Test.Tasty.Golden
 
-import qualified ANorm as A
-import CodegenMonad (generate)
-import Fresh (evalFresh)
-import Infer
-import qualified LowLevel as LL
-import Parsing (parse)
+import CmdLine
+import CmdLineArgs
 
 
-toLLVM :: String -> String -> IO BS.ByteString
-toLLVM filename input = evalFresh (A.aNormalizeProg lowLevel >>= generate) Map.empty
-  where
-    globals = Set.fromList ["printf"]
-    ast = either error id $ do
-      expr <- parse filename input
-      _ <- infer expr
-      return expr
-    lowLevel = LL.runConvert ast globals
+testArgs :: Args
+testArgs = defaultArgs { optimizationFlag = Just "-O3" }
 
 
 goldenTest :: String -> TestTree
 goldenTest tlFile = goldenVsString (takeBaseName tlFile) goldFile $ do
-  input <- readFile tlFile
-  llvm <- toLLVM tlFile input
-  _ <- readProcess "llc-9" ["-O2", "-filetype=asm", "-o", asmFile] $ BSU.toString llvm
-  _ <- readProcess "clang" ["-c", "runtime.c"] ""
-  _ <- readProcess "clang" [asmFile, "runtime.o", "-o", exeFile] ""
-  result <- BLU.fromString <$> readProcess ("./" ++ exeFile) [] ""
-  _ <- removeFile asmFile
-  _ <- removeFile exeFile
-  return result
+  either error BL.fromStrict <$> produceOutput args
   where
     goldFile = replaceExtension tlFile ".gold"
-    asmFile = replaceExtension tlFile ".s"
-    exeFile = replaceExtension tlFile ".out"
+    args = testArgs { inputFile = Just tlFile }
 
 
 findExecutionTests :: IO TestTree
